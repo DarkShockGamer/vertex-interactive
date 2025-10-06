cb8a5a651049eac3542768bd4eceafa38667ff53
DarkShockGamer <blackshocktrooper@gmail.com>
2025-10-06 16:42:35 -0500

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-// Smooth scroll
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', e => {
-    e.preventDefault();
-    document.querySelector(anchor.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
-    });
-  });
-});
